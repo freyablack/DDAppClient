226e65a12a741853e516268d229814c5eb54c3b5
Freya Black <freyablackdev@gmail.com>
2021-09-26 14:48:32 -0400

    fixing 2

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,8 @@ switch(window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:3000';
         break;
-        case 'https://fab-character-app-client.herokuapp.com':
-            APIURL = 'https://fab-character-app-server.herokuapp.com'
+        case 'fab-character-app-client.herokuapp.com':
+            APIURL = 'fab-character-app-server.herokuapp.com'
 }
 
 export default APIURL;
